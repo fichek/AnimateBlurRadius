203bc76c379214a5a71d88dcda6b0b7262f6f580
Filip Radelic <filip@me.com>
2016-06-20 00:00:58 +0200

    Initial Commit

---
+++ BlurDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BlurDemo
+//
+//  Created by Filip Radelic on 20.06.2016..
+//  Copyright © 2016. Filip Radelic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
